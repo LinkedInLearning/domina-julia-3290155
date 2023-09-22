2a5c8c82a9b1e9dca621325990344376f701412b
Lincy Gonzalez <lincgr@gmail.com>
2023-09-22 20:39:58 +0200

    02_04 Exportar datos en un archivo Excel en Julia

---
+++ 02_04/02_04.jl
@@ -0,0 +1,7 @@
+using DataFrames
+df = DataFrame(
+    nombre=["Luis", "Ana", "Sebastián", "Sofía", "Mateo", "Camila", "Valentín", "Isabella", "Lucía", "Diego", "Valeria"],
+    edad=[15, 30, 32, 41, 47, 23, 20, 18, 17, 25, 14],
+    ciudad=["Buenos Aires", "Lima", "Ciudad de México", "Santiago", "Alajuela", "Nueva York", "Toronto", "Río de Janeiro", "Montevideo", "Miami", "Vancouver"])
+
+ruta_archivo = "./clientes.xlsx"
