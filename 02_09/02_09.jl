35ec85b62da8f06218fb742cef115984c96f3093
Lincy Gonzalez <lincgr@gmail.com>
2023-09-23 11:41:27 +0200

    02_09 Ordenamiento de filas en Julia

---
+++ 02_09/02_09.jl
@@ -0,0 +1,6 @@
+using DataFrames
+
+df_estudiantes = DataFrame(
+    nombre=["Luis", "Ana", "Sebastián", "Sofía", "Mateo", "Camila", "Valentín", "Isabella", "Lucía", "Diego", "Valeria"],
+    nota=[45, 70, 85, 73, 49, 89, 90, 63, 71, 76, 56]
+)
