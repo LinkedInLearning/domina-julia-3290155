5cc83a9b2f4f49f8803573ed2f2e140d3b3c855e
Lincy Gonzalez <lincgr@gmail.com>
2023-09-23 13:18:42 +0200

    03_06 Seleccionar datos de un dataframe por medio de Query.jl en Julia

---
+++ 03_06/03_06.jl
@@ -0,0 +1,7 @@
+using DataFrames, Query
+
+df_cliente = DataFrame(
+    nombre=["Luis", "Ana", "Sebastián", "Sofía", "Mateo", "Camila", "Valentín", "Isabella", "Lucía", "Diego", "Valeria"],
+    compra=[8500, 11000, 9200, 13500, 7800, 10500, 8800, 12200, 10000, 7500, 12000],
+    ciudad=["Alajuela", "Cartago", "Heredia", "Alajuela", "Alajuela", "Heredia", "Alajuela", "Heredia", "Cartago", "Cartago", "Cartago"]
+)
