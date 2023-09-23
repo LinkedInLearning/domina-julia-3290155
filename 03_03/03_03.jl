3e35e2c0cd4cb4d6ece71e9826433e6b84939795
Lincy Gonzalez <lincgr@gmail.com>
2023-09-23 12:39:46 +0200

    03_03 Unir dos dataframe por medio de un inner join en Julia

---
+++ 03_03/03_03.jl
@@ -0,0 +1,11 @@
+using DataFrames
+
+df_estudiantes = DataFrame(
+    identicador=[1, 2, 3, 4, 5, 6], 
+    nombre=["Luis", "Ana", "Sebastián", "Sofía", "Mateo", "Camila"]
+)
+
+df_ciudad = DataFrame(
+    identicador=[2,3,4,5,7,8], 
+    ciudad=["Buenos Aires", "Lima", "Ciudad de México", "Santiago", "Alajuela", "Nueva York"]
+)
