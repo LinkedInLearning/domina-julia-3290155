a97d0318b217f01e97b50bbeaa78de56b71cbbda
Lincy Gonzalez <lincgr@gmail.com>
2023-09-23 12:12:03 +0200

    02_11 Unir dataframes en Julia

---
+++ 02_11/02_11.jl
@@ -0,0 +1,13 @@
+using DataFrames
+
+df_enero = DataFrame(
+        factura = [1, 2, 3, 4, 5, 6, 7, 8], 
+        venta=[10000, 7500, 12000, 9000, 11000, 8500, 9500, 13000])
+
+df_febrero = DataFrame(
+    factura = [9, 10, 11, 12, 13, 14, 15, 16], 
+    venta=[8500, 11000, 9200, 13500, 7800, 10500, 8800, 12200])
+
+df_union = vcat(df_enero, df_febrero)
+
+println(df_union)
