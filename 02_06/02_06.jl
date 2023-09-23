e46b94739df1f420ff5ad87b5fe1290c3789af4e
Lincy Gonzalez <lincgr@gmail.com>
2023-09-23 10:31:40 +0200

    02_06 Eliminar los valores faltantes de dataframe en Julia

---
+++ 02_06/02_06.jl
@@ -0,0 +1,16 @@
+using DataFrames
+
+df = DataFrame(
+    nombre=["Luis", "Ana", "Sebastián", "Sofía", "Mateo", "Camila", "Valentín", "Isabella", "Lucía", "Diego", "Valeria"],
+    edad=[15, 30, 32, 41, 47, 23, 20, 18, 17, 25, 14],
+    ciudad=["Buenos Aires", missing, "Ciudad de México", missing, "Alajuela", "Nueva York", missing, "Río de Janeiro", "Montevideo", "Miami", "Vancouver"])
+
+total_faltantes = 0
+
+for columna in eachcol(df)
+    global total_faltantes += sum(ismissing.(columna))
+end
+
+println("El total de valores faltantes en el dataframe es: $total_faltantes")
+
+println(df)
