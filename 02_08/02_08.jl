cc1056b46ac70e87e46c79455bb719abbc602d20
Lincy Gonzalez <lincgr@gmail.com>
2023-09-23 11:20:15 +0200

    02_08 División de un dataframe en grupos en Julia

---
+++ 02_08/02_08.jl
@@ -0,0 +1,7 @@
+using DataFrames
+
+df = DataFrame(
+    nombre=["Luis", "Ana", "Sebastián", "Sofía", "Mateo", "Camila", "Valentín", "Isabella", "Lucía", "Diego", "Valeria"],
+    edad=[15, 30, 32, 18, 47, 23, 20, 35, 17, 76, 14],
+    ciudad=["Alajuela", "Cartago", "Heredia", "Alajuela", "Alajuela", "Heredia", "Alajuela", "Heredia", "Cartago", "Cartago", "Cartago"]
+)
